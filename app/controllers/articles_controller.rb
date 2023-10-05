1e9760110e5d6131e70a84a3605f3a93b301280b
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-05 16:23:05 +0100

    Complete 7.2 Resourceful Routing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
   def index
-	@articles = Article.all
+		@articles = Article.all
   end
 
   def show
-	@article = Article.find(params[:id])
+		@article = Article.find(params[:id])
   end
 end
